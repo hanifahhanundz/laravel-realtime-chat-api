01c7ff46a51702ac7d0a9bb2b3dbf2bcc3bd2cb2
Hijrah Assalam <31386955+hijrahassalam@users.noreply.github.com>
2026-05-08 23:41:04 +0800

    feat: add Laravel Sanctum API authentication (install:api)

---
+++ routes/api.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\MessageController;
+use App\Http\Controllers\Api\RoomController;
+use Illuminate\Support\Facades\Route;
+
+// Public auth routes
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+// Protected routes
+Route::middleware('auth:sanctum')->group(function () {
+    // Auth
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/user', [AuthController::class, 'user']);
+
+    // Rooms
+    Route::get('/rooms', [RoomController::class, 'index']);
+    Route::post('/rooms', [RoomController::class, 'store']);
+    Route::get('/rooms/{room}', [RoomController::class, 'show']);
+    Route::delete('/rooms/{room}', [RoomController::class, 'destroy']);
+
+    // Messages
+    Route::get('/rooms/{room}/messages', [MessageController::class, 'index']);
+    Route::post('/rooms/{room}/messages', [MessageController::class, 'store']);
+    Route::post('/rooms/{room}/typing', [MessageController::class, 'typing']);
+    Route::patch('/rooms/{room}/messages/{message}/read', [MessageController::class, 'markRead']);
+});
